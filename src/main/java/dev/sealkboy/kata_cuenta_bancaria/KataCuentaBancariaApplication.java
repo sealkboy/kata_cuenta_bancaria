4c632d89dc2e9060849bf3fc250fe176c1331a4b
unknown <sealkevinboyabad@gmail.com>
2024-12-04 22:54:16 -0800

    Primer commit

---
+++ src/main/java/dev/sealkboy/kata_cuenta_bancaria/KataCuentaBancariaApplication.java
@@ -0,0 +1,13 @@
+package dev.sealkboy.kata_cuenta_bancaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataCuentaBancariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KataCuentaBancariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/sealkboy/kata_cuenta_bancaria/KataCuentaBancariaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sealkboy.kata_cuenta_bancaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KataCuentaBancariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
