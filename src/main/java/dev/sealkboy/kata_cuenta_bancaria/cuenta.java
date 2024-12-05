a6df3ef97d111695013709e5809e157a8eec5382
unknown <sealkevinboyabad@gmail.com>
2024-12-04 23:58:40 -0800

    Implementados atributos y constructor de clase 'cuenta'

--- src/main/java/dev/sealkboy/kata_cuenta_bancaria/cuenta.java
+++ src/main/java/dev/sealkboy/kata_cuenta_bancaria/cuenta.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KataCuentaBancariaApplication {
+public class cuenta {
 
 	public static void main(String[] args) {
-		SpringApplication.run(KataCuentaBancariaApplication.class, args);
+		SpringApplication.run(cuenta.class, args);
 	}
 
 }
